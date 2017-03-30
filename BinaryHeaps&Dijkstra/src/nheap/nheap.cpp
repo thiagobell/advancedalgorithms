e9f3bf96d376abba29e08364fd99bef4fc257e28
Thiago <thiago@tim>
2017-03-30 13:51:10 -0300

    functional heap

--- BinaryHeaps&Dijkstra/src/nheap/nheap.cpp
+++ BinaryHeaps&Dijkstra/src/nheap/nheap.cpp
@@ -2,41 +2,46 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
+#include <string>
 
-Nheap::Nheap(int n, int maxsize)
+template <class T>
+Nheap<T>::Nheap(int n, int maxsize)
 {
 this->n = n;
 this->maxsize = maxsize;
 }
 
-unsigned Nheap::get_index_child(unsigned parent_index, unsigned child_num)
+template <class T>
+unsigned Nheap<T>::get_index_child(unsigned parent_index, unsigned child_num)
 {
 return parent_index*n + child_num;
 }
 
-unsigned Nheap::get_index_parent(unsigned elem_index)
+template <class T>
+unsigned Nheap<T>::get_index_parent(unsigned elem_index)
 {
 return std::floor((elem_index-1)/n);
 }
 
-
-void Nheap::insert(std::pair<unsigned int, unsigned int> elem) 
+template <class T>
+void Nheap<T>::insert(hnode<T> elem) 
 {
   unsigned int position = data.size(); //index at which the elem will be inserted
   data.push_back(elem);
   heapify_up(position);
 }
 
-void Nheap::heapify_up(unsigned int position_elem)
+template <class T>
+void Nheap<T>::heapify_up(unsigned int position_elem)
 {
   if(position_elem == 0) //is root
     return;
   unsigned int position_parent =  get_index_parent(position_elem);
-  unsigned int key_parent = data[position_parent].first;
-  unsigned int key_elem = data[position_elem].first;
+  unsigned int key_parent = data[position_parent].key;
+  unsigned int key_elem = data[position_elem].key;
 
   if(key_parent > key_elem) {
-    std::pair<unsigned int, unsigned int> temp;
+    hnode<T> temp;
     temp = data[position_parent];
     data[position_parent] = data[position_elem];
     data[position_elem] = temp;
@@ -44,7 +49,8 @@ void Nheap::heapify_up(unsigned int position_elem)
   }
 }
 
-void Nheap::heapify_down(unsigned int position_elem) {
+template <class T>
+void Nheap<T>::heapify_down(unsigned int position_elem) {
 
   unsigned int position_first_child = get_index_child(position_elem, 1);
 
@@ -59,57 +65,69 @@ void Nheap::heapify_down(unsigned int position_elem) {
 
   if(number_children == 1) {
     //element has only one child
+    if(data[position_elem].key > data[position_first_child].key){
+      hnode<T> temp = data[position_elem];
+      data[position_elem] = data[position_first_child];
+      data[position_first_child] = temp;
+      heapify_down(position_first_child);
+    }
   } else {
     //there is more than one child
     unsigned lowest_key, lowest_key_position;
-    lowest_key = data[position_first_child].first;
+    lowest_key = data[position_first_child].key;
     lowest_key_position = position_first_child;
     for(unsigned i=position_first_child+1; i < position_first_child+number_children;i++){
-      unsigned key = data[i].first;
+      unsigned key = data[i].key;
       if(key < lowest_key){
 	lowest_key = key;
 	lowest_key_position = i;
       }
     }
 
-    if(data[position_elem].first > lowest_key) {
-    
-    //swap elements if parent has a key larger then one of the smallest children
-    std::pair<unsigned int, unsigned int> temp = data[position_elem];
-    data[position_elem] = data[lowest_key_position];
-    data[lowest_key_position] = temp;
-    heapify_down(lowest_key_position);
+    if(data[position_elem].key > lowest_key) {
+      //swap elements if parent has a key larger then one of the smallest children
+      hnode<T> temp = data[position_elem];
+      data[position_elem] = data[lowest_key_position];
+      data[lowest_key_position] = temp;
+      heapify_down(lowest_key_position);
     }
   }
   
 
 }
+
+
 //finds internal vector index for a given key
-unsigned int Nheap::find_key_index(unsigned int key)
+template <class T>
+unsigned int Nheap<T>::find_elem_index(T value)
 {
   int i=0;
   while(i < data.size()) {
-    if(data[i].first == key)
+    if(data[i].value == value)
       return i;
     i++;
   }
   throw "key not found";
 }
-  
-void Nheap::update(unsigned int old_key, unsigned int new_key)
+
+
+template <class T>
+void Nheap<T>::update_key(T value, unsigned int new_key)
 {
-  unsigned int pos = find_key_index(old_key);
+  unsigned int pos = find_elem_index(value);
+  unsigned int old_key = data[pos].key;
   if(new_key < old_key) {
-    data[pos].first = new_key;
+    data[pos].key = new_key;
     heapify_up(pos);
   } else {
-    data[pos].first = new_key;
+    data[pos].key = new_key;
     heapify_down(pos);
   }
 }
 
 //error if size of tree is zero
-std::pair<unsigned int, unsigned int> Nheap::get_min()
+template <class T>
+hnode<T> Nheap<T>::get_min()
 {
   if(data.size() > 1) {
     return data[0];
@@ -118,8 +136,8 @@ std::pair<unsigned int, unsigned int> Nheap::get_min()
   //throws exception
 }
 
-
-void Nheap::print_heap()
+template <class T>
+void Nheap<T>::print_heap()
 {
   int node_count = 0;
   int num_nodes_at_level = 1;
@@ -128,24 +146,43 @@ void Nheap::print_heap()
     
     if(nodes_printed_at_level < num_nodes_at_level) {
       nodes_printed_at_level++;     
-      std::cout << data[node_count].first << " ";
+      std::cout << data[node_count].key << " ";
     } else {
       nodes_printed_at_level = 1;
       num_nodes_at_level *= n;
-      std::cout << "\n" << data[node_count].first << " ";
+      std::cout << "\n" << data[node_count].key << " ";
     }
     node_count++;
   }
   std::cout << "\n";
 }
 
+template <class T>
+void Nheap<T>::delete_elem(T value)
+{
+  unsigned int array_index = find_elem_index(value);
+  data[array_index] = data[data.size()-1];
+  data.pop_back();
+  heapify_down(array_index);
+}
 
+template <class T>
+hnode<T> Nheap<T>::delete_min()
+{
+  hnode<T> temp = get_min();
+  data[0] = data[data.size()-1];
+  data.pop_back();
+  heapify_down(0);
+  return temp;
+}
+/*
 int main()
 {
+  
   std::cout << "initializing heap\n";
-  Nheap myheap(2, 200);
+  Nheap<std::string> myheap(2, 200);
   std::cout << "done./n";
-  std::pair<unsigned, unsigned> pair1(30,2), pair2(100,3),pair3(1,4),min;
+  hnode<std::string> pair1(30,"hello"), pair2(100,"its"),pair3(1,"me"),min;
   std::cout << "inserting pair1...\n";
   myheap.insert(pair1);
   std::cout << "inserting pair2...\n";
@@ -155,16 +192,22 @@ int main()
   std::cout << "done.\n";
   myheap.print_heap();
   min = myheap.get_min();
-  std::cout << "minimum key is " << min.first;
+  std::cout << "minimum key is " << min.key;
 
   std::cout << "updating key 1 to 2\n";
-  myheap.update(1,2);
+  myheap.update_key("me",2);
   min = myheap.get_min();
-  std::cout << "minimum key is " << min.first << "\n";
+  std::cout << "minimum key and value are " << min.key <<" "<<min.value << "\n";
   
   std::cout << "updating key 30 to 0\n";
-  myheap.update(30,0);
+  myheap.update_key("hello",0);
   min = myheap.get_min();
-  std::cout << "minimum key is " << min.first << "\n";
+  std::cout << "minimum key and value are " << min.key <<" "<<min.value << "\n";
   myheap.print_heap();
+
+  std::cout << "delete key 0\n";
+  myheap.delete_elem("hello");
+  myheap.print_heap();
+  
 }
+*/
