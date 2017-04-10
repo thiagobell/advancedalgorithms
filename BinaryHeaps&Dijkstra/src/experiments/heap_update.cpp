eb02c0e2dc89eaff094fbe4a3647156ae864fc24
Thiago Bell <thibef@gmail.com>
2017-04-10 12:39:32 -0300

    s

--- BinaryHeaps&Dijkstra/src/experiments/heap_update.cpp
+++ BinaryHeaps&Dijkstra/src/experiments/heap_update.cpp
@@ -8,29 +8,35 @@
 int main()
 {
   unsigned n = 2;
-  Nheap<unsigned int> heap(n, 2);
   unsigned int count = 0;
 
   for(unsigned i = 1; i <21; i++) {
-
-    std::chrono::system_clock::time_point t = std::chrono::system_clock::now();
-
-    unsigned int two_i = std::pow(2, i);
-    unsigned int value = two_i +1;
-    for(unsigned int j = 0; j < two_i -1; j++){
-      heap.insert(hnode<unsigned int>(value, value));
-    }
-    value +=1;
-    for(unsigned int j = 0; j < two_i ; j++){
-      heap.insert(hnode<unsigned int>(value, value));
-    }
-    unsigned int new_value = two_i;
-    for(unsigned int j = 0; j < two_i ; j++){
-      heap.update_key(value, new_value);
-      std::cout << new_value << "\n";
-      new_value--;
-    }
     
+    Nheap<unsigned int> heap(n, 2);
+    std::chrono::system_clock::time_point t = std::chrono::system_clock::now();
+    unsigned int next_value = 1;
+    unsigned int two_i = std::pow(2, i);
+    unsigned int priority = two_i +1;
+    for(unsigned int j = 0; j < two_i -1; j++){
+      heap.insert(hnode<unsigned int>(priority, next_value));
+      next_value++;
+      //      std::cout << " inserting priority " << priority << "value "<< next_value-1<<"\n";
+    }
+    priority +=1;
+    for(unsigned int j = 0; j < two_i ; j++){
+      heap.insert(hnode<unsigned int>(priority, next_value));
+      next_value++;
+      
+      //      std::cout << " inserting priority " << priority << "value "<< next_value-1<<"\n";
+    }
+    //heap.print_heap();
+    unsigned int new_priority = two_i;
+    for(unsigned int j = next_value-two_i; j < next_value ; j++){
+      //std::cout <<"updating value = "<<j<< " priority <<" << new_priority <<"\n";
+      heap.update_key(j, new_priority);
+      new_priority--;
+    }
+    //heap.print_heap();
     std::cout << "i="<<i<<" time=" << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now()-t).count() << " swap count= "<< heap.num_swaps <<  std::endl;
 
     heap.num_swaps = 0;
