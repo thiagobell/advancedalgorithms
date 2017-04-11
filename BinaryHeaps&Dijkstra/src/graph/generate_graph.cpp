da8094928cbf32ab40d27f7dfc593369b07ef489
Thiago <thiago@tim>
2017-04-11 11:55:14 -0300

    more efficient graph generation

--- BinaryHeaps&Dijkstra/src/graph/generate_graph.cpp
+++ BinaryHeaps&Dijkstra/src/graph/generate_graph.cpp
@@ -6,66 +6,45 @@
  *   \version $Id: emacs 2872 2009-01-31 01:46:50Z ritt $
  *   \date Time-stamp: <2011-08-24 15:17:49 ritt>
  */
-#include <iostream>
 #include <fstream>
-#include <cassert>
+#include <iostream>
+#include <vector>
+//#include <chrono>
+#include <cmath>
+#include <cstdlib>
+
 using namespace std;
-
-#include <boost/graph/adjacency_list.hpp>
-#include <boost/graph/connected_components.hpp>
-#include <boost/graph/dijkstra_shortest_paths.hpp>
-using namespace boost;
-
-// information stored in vertices
-struct VertexInformation {
-  unsigned component;
-};
 
 // information stored in edges
 struct EdgeInformation {
+  unsigned o,d;
   unsigned weight;
+  EdgeInformation(unsigned o_, unsigned d_, unsigned w_ ) {
+    o = o_;
+    d = d_;
+    weight = w_;
+  }
 };
 
 const unsigned maxweight = 1000;
 
-// graph is an adjacency list represented by vectors
-typedef adjacency_list<vecS, vecS, directedS,VertexInformation,EdgeInformation> Graph;
-typedef graph_traits<Graph>::vertex_descriptor Node;
-typedef graph_traits <Graph>::edge_descriptor Edge;
 
 int generate_graph(int n, double p,const char *filepath_output) {
-
+  vector<EdgeInformation> edges;
   srand48(time(0));
   ofstream outfile;
   outfile.open(filepath_output);
-  // (1) generate random graph
-  Graph g;
 
-  for(unsigned i=0; i<n; i++)
-    add_vertex(g);
-  cout<<"vertexes added\n";
-  for(unsigned i=0; i<n; i++)
-    for(unsigned j=0; j<n; j++)
-      if (i != j && drand48() < p) {
-	Edge e = add_edge(i,j,g).first;
-	g[e].weight = lrand48()%maxweight;
-      }
-  cout << "here\n";
-
-  // (2) print example path
-  unsigned src = lrand48()%num_vertices(g);
-  unsigned dst = lrand48()%num_vertices(g);
-
-  vector<unsigned> dist(n);
-  vector<unsigned> pred(n);
-  dijkstra_shortest_paths(g,src,weight_map(get(&EdgeInformation::weight,g)).distance_map(&dist[0]).predecessor_map(&pred[0]));
-  cerr << "Distance between " << src+1 << " and " << dst+1 << " is " << dist[dst] << endl;
-
+  for(unsigned i=1; i<=n; i++)
+    for(unsigned j=1; j<=n; j++)
+      if (i != j && drand48() < p)
+	edges.push_back(EdgeInformation(i,j, lrand48()%maxweight));
+ 
   // (3) print out in DIMACS challenge format
-  outfile << "p sp " << num_vertices(g) << " " << num_edges(g) << endl;
-  graph_traits<Graph>::edge_iterator eb, ee;
-  for ( tie(eb, ee)=edges(g); eb != ee; eb++)
-    outfile << "a " << source(*eb,g)+1 << " " << target(*eb, g)+1 << " " << g[*eb].weight << endl;
+  outfile << "p sp " << n << " " << edges.size() << endl;
+ 
+  for (unsigned int ind =0; ind < edges.size(); ind++)
+    outfile << "a " << edges[ind].o << " " <<edges[ind].d << " " << edges[ind].weight;
 
   outfile.close();
 }
