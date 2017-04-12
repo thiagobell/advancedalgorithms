53edcbaf2dc403030d71a9c863c59a48bfd665c2
thibef <thiago@bell.pt>
2017-04-12 01:34:27 -0300

    treating infinity condition

--- BinaryHeaps&Dijkstra/src/main.cpp
+++ BinaryHeaps&Dijkstra/src/main.cpp
@@ -3,29 +3,26 @@
 #include <sstream>
 #include <iostream>
 #include "dijkstra/dijkstra.h"
-
+#include <limits>
 
 int main(int argc, char **argv) {
 std::vector<unsigned> distances;
+ if(argc < 3) {
+   std::cout << "error: Expecting origin destination pair\n";
+ }
 
-if(argc < 3) {
-std::cout << "error: Expecting origin destination pair\n";
-}
+ unsigned int o,d;
+ o = std::atoi(argv[1]);
+ d = std::atoi(argv[2]);
+ 
+ Graph g = Graph(std::cin);
+ distances = dijkstra(o,g,2);
+ 
+ if(distances[d-1]==std::numeric_limits<int>::max()){
+   std::cout << "inf\n";
+ } else {
+   std::cout << distances[d-1]<<"\n";
+ }
 
-unsigned int o,d;
-//try {
-o = std::atoi(argv[1]);
-d = std::atoi(argv[2]);
-
-  Graph g = Graph(std::cin);
-  distances = dijkstra(o,g,2);
-
-
-
-// }catch (const char* msg) {
-//std::cout  << msg << std::endl; 
-// }
-
-std::cout << distances[d-1]<<"\n";
 
 }
