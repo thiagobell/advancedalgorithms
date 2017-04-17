7c0547240b5b016575f10046d8f23fe940bb4578
thibef <thiago@bell.pt>
2017-04-17 00:37:30 -0300

    report improvements

--- BinaryHeaps&Dijkstra/results/fix_vertex/regression.R
+++ BinaryHeaps&Dijkstra/results/fix_vertex/regression.R
@@ -1,10 +1,10 @@
-d= read.csv(file="processed_data.csv", header=TRUE, sep=",")
-ff = (log10(d$m) + log10(d$n))
-l=lm(log10(d$time) ~ log10(d$m) )
+d= read.csv(file="fixed_edge.csv", header=TRUE, sep=",")
+
+l=lm(log10(d$time) ~ log10(d$n) )
 summary(l)
-with(d,plot(log10(d$m),log10(d$time)))
+with(d,plot(log10(d$n),log10(d$time)))
 abline(l)
 
-plotdata = cbind(log10(d$time), log10(d$m), fitted(l) )
-colnames(plotdata) = c("logtime", "logm", "pred" )
-write.csv(plotdata, "fix_vertex_lin_regression.csv",row.names=FALSE)
+plotdata = cbind(log10(d$time), log10(d$n), fitted(l) )
+colnames(plotdata) = c("logtime", "logn", "pred" )
+write.csv(plotdata, "fix_edge_lin_regression.csv",row.names=FALSE)
